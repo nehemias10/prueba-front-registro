6d32c593363a118214b03687ad6db914a43079a5
nehemias10 <nehemiasrivero1@gmail.com>
2022-09-08 01:29:35 +0000

    prueba

--- src/js/views/home.js
+++ src/js/views/home.js
@@ -3,13 +3,40 @@ import rigoImage from "../../img/rigo-baby.jpg";
 import "../../styles/home.css";
 
 export const Home = () => (
-	<div className="text-center mt-5">
-		<h1>Hello Rigo!</h1>
-		<p>
-			<img src={rigoImage} />
-		</p>
-		<a href="#" className="btn btn-success">
-			If you see this green button, bootstrap is working
-		</a>
+	<div>
+		<form>
+
+		<div class="form-outline mb-4">
+    <input type="" id="form2Example1" class="form-control" />
+    <label class="form-label" for="form2Example1">Name</label>
+  </div>
+  
+  <div class="form-outline mb-4">
+    <input type="email" id="form2Example1" class="form-control" />
+    <label class="form-label" for="form2Example1">Email address</label>
+  </div>
+
+  
+  <div class="form-outline mb-4">
+    <input type="password" id="form2Example2" class="form-control" />
+    <label class="form-label" for="form2Example2">Password</label>
+  </div>
+
+ 
+  <div class="row mb-4">
+    <div class="col d-flex justify-content-center">
+   
+      
+    </div>
+
+    
+  </div>
+
+  
+  <button type="button" class="btn btn-primary btn-block mb-4">Register</button>
+
+ 
+  
+</form>
 	</div>
 );
